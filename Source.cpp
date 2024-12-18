4f1ab9be72b75a806f949155a6e5fca2dd3d8723
9k82khd3i4v4ure56o9 <dmitriyminihanov@gmail.com>
2024-12-19 00:51:05 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <clocale>
+#include <cmath>
+using namespace std;
+
+double fact(int n)
+{
+	double res = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		res *= i;
+	}
+
+	return res;
+}
+
+double f(double x, int n) 
+{
+	if (n == 0) 
+	{
+		return 1;
+	}
+
+	double pre_p = f(x, n - 1);
+
+	double p = (2 * n + 1) / fact(n) * pow(x, 2 * n);
+
+	cout << n << "-е слагаемое = " << p << endl;
+
+	return pre_p + p;
+}
+
+int X, N;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "¬ведите x и n:" << endl;
+	cin >> X >> N;
+
+	while (N < 0)
+	{
+		cout << "n не может быть меньше 0. ¬ведите n:" << endl;
+		cin >> N;
+	}
+
+	double result = f(X, N);
+
+	cout << result;
+	
+	return 0;
+}
